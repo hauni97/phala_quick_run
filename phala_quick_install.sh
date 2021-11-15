165526e7e41b4bc1420d9e3823e1de0951672ee4
David <david.h99@hotmail.de>
2021-11-15 13:17:04 +0100

    fixes

--- phala_quick_install.sh
+++ phala_quick_install.sh
@@ -73,13 +73,14 @@ else
     echo Yes
     echo -n "1. In a seperate shell on same host enter 'curl https://sh.rustup.rs -sSf | sh'\n2.Then enter 'source ~/.cargo/env'\nPress any key to continue when done" 
     while [ true ] ; do
-    read -t 3 -n 1
+    read  3 -n 1
     if [ $? = 0 ] ; then
     exit ;
     else
     echo "waiting for the keypress"
-    echo No
     fi
+    done
+    
 
 fi
 
@@ -89,34 +90,42 @@ echo ""
 echo "[+] Installing SGX SDK"
 echo  -n "1. In a seperate shell on same host enter 'wget https://download.01.org/intel-sgx/sgx-linux/2.15/distro/ubuntu20.04-server/sgx_linux_x64_sdk_2.15.100.3.bin'\nPress any key to continue when done"
 while [ true ] ; do
-read -t 3 -n 1
+read  3 -n 1
 if [ $? = 0 ] ; then
 exit ;
 else
 echo "waiting for the keypress"
+fi
+done
 echo -n "In seperate shell enter'chmod +x sgx_linux_x64_sdk_2.15.100.3.bin'\nPress any key to continue when done"
 while [ true ] ; do
-read -t 3 -n 1
+read  3 -n 1
 if [ $? = 0 ] ; then
 exit ;
 else
 echo "waiting for the keypress"
+fi
+done
 echo -n "In seperate shell enter 'echo -e 'no\n/opt/intel' | sudo ./sgx_linux_x64_sdk_2.15.100.3.bin"
 while [ true ] ; do
-read -t 3 -n 1
+read  3 -n 1
 if [ $? = 0 ] ; then
 exit ;
 else
+echo "waiting for the keypress"
+fi
+done
 echo "waiting for the keypress"
 yes | rm sgx_linux_x64_sdk_2.15.100.3.bin
 echo -n "[+] To configure your current shell with Intel SGX SDK , run in the seperate shell: \nsource /opt/intel/sgxsdk/environment\nhit any key to proceed"
 while [ true ] ; do
-read -t 3 -n 1
+read  3 -n 1
 if [ $? = 0 ] ; then
 exit ;
 else
 echo "waiting for the keypress"
-
+fi
+done
 
 
 
@@ -167,5 +176,5 @@ node --version
 yarn --version
 # 2.1.1"
 
-
-
+fi
+done
